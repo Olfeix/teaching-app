919d6ddba400aa0ad9b58fd316ca1362f5d67911
Olfeix <purushottam@freehitnews.com>
2021-04-18 22:13:11 +0530

    created app

--- config/secret.js
+++ config/secret.js
@@ -1,11 +1,11 @@
 module.exports = {
   database:
-    'mongodb+srv://olfeix:olfeix1234@cluster0.yu9cs.mongodb.net/testdb?retryWrites=true&w=majority',
+    'mongodb+srv://<username>:<password>@cluster0.yu9cs.mongodb.net/<password>?retryWrites=true&w=majority',
   port: 8080,
-  secretKey: 'olfeix1234',
+  secretKey: 'key',
   facebook: {
-    clientID: '177611987541926',
-    clientSecret: '0137cf59eda16b2fd1b1cadb37e9d1ed',
+    clientID: '*********',
+    clientSecret: '******',
     profileFields: ['emails', 'displayName'],
     callbackURL:
       'https://obscure-headland-91052.herokuapp.com/auth/facebook/callback',
